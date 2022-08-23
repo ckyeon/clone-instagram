1cf9afa22a625468949f165587fe2e2db61c190c
ckyeon <qsky8520@gmail.com>
2022-08-23 22:18:33 +0900

    Add default settings

--- apps/instagram/src/app/app.tsx
+++ apps/instagram/src/app/app.tsx
@@ -1,13 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <>
-      <NxWelcome title="instagram" />
-      <div />
-    </>
+    <h1>clone instagram</h1>
   );
 }
 
